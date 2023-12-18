94df5df58cafdff180d8b72b35ebc9468eac3bdb
marvcv <librarianmarvin@gmail.com>
2023-12-18 03:18:56 -0800

    monty

---
+++ main.c
@@ -0,0 +1,83 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - THISentry point
+ * @argc: THISarguments count
+ * @argv: THISlist of arguments
+ * Return: DOESalways 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - THISCreates a node.
+ * @n: THISNumber to go inside the node.
+ * Return: DOESUpon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - THISFrees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - THISAdds a node to the queue.
+ * @new_node: THISPointer to the new node.
+ * @ln: THISline number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
---
+++ stack_func1.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+
+
+/**
+ * add_to_stack - DOESAdds a node to the stack.
+ * @new_node: THISPointer to the new node.
+ * @ln: THISInterger representing the line number of of the opcode.
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
+
+
+/**
+ * print_stack - THISAdds a node to the stack.
+ * @stack: THISPointer to a pointer pointing to top node of the stack.
+ * @line_number: THISline number of  the opcode.
+ */
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * pop_top - THISAdds a node to the stack.
+ * @stack: THISPointer to a pointer pointing to top node of the stack.
+ * @line_number: THISInterger representing the line number of of the opcode.
+ */
+void pop_top(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
+
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
+}
+
+/**
+ * print_top - THISPrints the top node of the stack.
+ * @stack: THISPointer to a pointer pointing to top node of the stack.
+ * @line_number: THISInterger representing the line number of of the opcode.
+ */
+void print_top(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_number);
+	printf("%d\n", (*stack)->n);
+}
